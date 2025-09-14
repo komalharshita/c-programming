fd4396613d6347d8ccdd44887bb97e29ad9cc612
komal harsh27 <komal.sony234@gmail.com>
2025-09-14 14:50:40 +0530

    did loops »

---
+++ 4. loops/num-combinations.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a, b, c ;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("All possible combinations of %d ,%d, %d are : \n", a, b, c);
+
+    for (int i = a; i <= c; i++) {
+        for (int j = a; j <= c; j++) {
+            for (int k = a; k <= c; k++) {
+                printf("%d %d %d\n", i, j, k);
+            }
+        }
+    }
+
+
+    return 0 ;
+}
--- 4. loops/while loop.c
+++ 4. loops/while loop.c
@@ -20,5 +20,19 @@ int main ()
         i-- ;
     }
 
+    int u = 5 ;
+    while ( u>=1){
+        printf("Make the computer literate!\n");
+        i = i+1 ;
+    }
+
+    float a = 10.0 ;
+    
+    while ( a<= 10.5){
+        printf("Raindrops on roses...");
+        printf("...and whiskers on kittens\n");
+        a = a + 0.1 ;
+    }
+
     return 0;
 }
